d9233c72ffe306c7e24a950b102ed1bb671b890c
dens-stein <github@baersteinbacher.de>
2025-06-07 22:29:17 +0200

    logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <svg viewBox="0 0 200 80" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-    <text x="0" y="30" textLength="200" lengthAdjust="spacingAndGlyphs" font-size="32" fill="currentColor" font-family="sans-serif">FOH</text>
-    <text x="0" y="70" textLength="200" lengthAdjust="spacingAndGlyphs" font-size="32" fill="currentColor" font-family="sans-serif">ULTRAS</text>
+    <text x="0" y="30" textLength="180" font-weight="700" lengthAdjust="spacingAndGlyphs" font-size="32" fill="currentColor" font-family="sans-serif">FOH</text>
+    <text x="0" y="70" textLength="180" font-weight="700" lengthAdjust="spacingAndGlyphs" font-size="32" fill="currentColor" font-family="sans-serif">ULTRAS</text>
 </svg>
